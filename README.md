e0d019326e55ceceebadb00137d1a6e5e1a51b99
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2021-05-12 18:35:13 +0530

    First initial commit of the  project

---
+++ README.md
@@ -0,0 +1,55 @@
+# Frontend Mentor - Intro component with sign up form
+
+![Design preview for the Intro component with sign up form coding challenge](./design/desktop-preview.jpg)
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Active State Screenshot](#active-state-screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the `form` is submitted if:
+  - Any `input` field is empty. The message for this error should say _"[Field Name] cannot be empty"_
+  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say _"Looks like this is not an email"_
+
+### Screenshot
+
+![](./screenshot_sign_up.png)
+
+### Active State Screenshot
+
+![](./screenshot_active_state.png)
+
+### Links
+
+- Solution URL: [Intro Component With Sign Up Form](https://www.frontendmentor.io/challenges/huddle-landing-page-with-alternating-feature-blocks-5ca5f5981e82137ec91a5100/hub/html5-css3-flex-sass-gsap-wb2FMUKKv)
+- Live Site URL: [Live Site](https://huddle-alternate-blocks.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Sass (Syntactically awesome stylesheets)
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@Pritam-Tirpude](https://www.frontendmentor.io/profile/Pritam-Tirpude)
+- Twitter - [@ptirpude1991](https://twitter.com/ptirpude1991)
---
+++ js/app.js
@@ -0,0 +1,84 @@
+const submitButton = document.querySelector("#submitBtn");
+
+const firstNameInput = document.querySelector("#firstname");
+const errorIconOne = document.querySelector("#ErrorIconOne");
+const labelErrorFirstname = document.querySelector("#ErrorFirstName");
+
+const lastNameInput = document.querySelector("#lastname");
+const errorIconTwo = document.querySelector("#ErrorIconTwo");
+const labelErrorLastname = document.querySelector("#ErrorLastName");
+
+const validRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const emailInput = document.querySelector("#email");
+const errorIconThree = document.querySelector("#ErrorIconThree");
+const labelErrorEmail = document.querySelector("#ErrorEmail");
+
+const passwordInput = document.querySelector("#password");
+const errorIconFour = document.querySelector("#ErrorIconFour");
+const labelErrorPassword = document.querySelector("#ErrorPassword");
+
+submitButton.addEventListener("click", checkErrors);
+
+function checkErrors(e) {
+  e.preventDefault();
+  //FirstName Check input value
+  if (firstNameInput.value === "") {
+    labelErrorFirstname.style.display = "block";
+    errorIconOne.style.visibility = "visible";
+    firstNameInput.style.border = "1px solid hsl(0, 100%, 74%)";
+    labelErrorFirstname.style.color = "hsl(0, 100%, 74%)";
+    labelErrorFirstname.innerText = "First Name cannot be empty";
+  } else {
+    firstNameInput.style.border = "1px solid yellowgreen";
+    labelErrorFirstname.style.display = "none";
+    errorIconOne.style.visibility = "hidden";
+  }
+
+  //Lastname check input value
+  if (lastNameInput.value === "") {
+    labelErrorLastname.style.display = "block";
+    errorIconTwo.style.visibility = "visible";
+    lastNameInput.style.border = "1px solid hsl(0, 100%, 74%)";
+    labelErrorLastname.style.color = "hsl(0, 100%, 74%)";
+    labelErrorLastname.innerText = "Last Name cannot be empty";
+  } else {
+    lastNameInput.style.border = "1px solid yellowgreen";
+    labelErrorLastname.style.display = "none";
+    errorIconTwo.style.visibility = "hidden";
+  }
+
+  //Email Validation check
+  if (emailInput.value === "") {
+    labelErrorEmail.style.display = "block";
+    errorIconThree.style.visibility = "visible";
+    emailInput.style.border = "1px solid hsl(0, 100%, 74%)";
+    labelErrorEmail.style.color = "hsl(0, 100%, 74%)";
+    labelErrorEmail.innerText = "Email Address cannot be empty";
+  } else if (!emailInput.value.match(validRegex)) {
+    labelErrorEmail.style.display = "block";
+    errorIconThree.style.visibility = "visible";
+    emailInput.style.border = "1px solid hsl(0, 100%, 74%)";
+    emailInput.style.color = "hsl(0, 100%, 74%)";
+    labelErrorEmail.style.color = "hsl(0, 100%, 74%)";
+    labelErrorEmail.innerText = "Looks like this is not an email";
+  } else {
+    emailInput.style.border = "1px solid yellowgreen";
+    emailInput.style.color = "hsl(249, 10%, 26%)";
+    labelErrorEmail.style.display = "none";
+    errorIconThree.style.visibility = "hidden";
+  }
+
+  //Password check
+  if (passwordInput.value === "") {
+    labelErrorPassword.style.display = "block";
+    errorIconFour.style.visibility = "visible";
+    passwordInput.style.border = "1px solid hsl(0, 100%, 74%)";
+    labelErrorPassword.style.color = "hsl(0, 100%, 74%)";
+    labelErrorPassword.innerText = "Password cannot be empty";
+  } else {
+    passwordInput.style.border = "1px solid yellowgreen";
+    labelErrorPassword.style.display = "none";
+    errorIconFour.style.visibility = "hidden";
+  }
+}
